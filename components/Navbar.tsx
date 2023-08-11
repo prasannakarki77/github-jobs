b371a1e94f1bbd1c6fd9b8983a61dfcf62f67e3a
prasannakarki77 <prasanna.karki77@gmail.com>
2023-08-11 07:21:54 +0545

    added css variables for theme

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,12 +1,18 @@
 import { ModeToggle } from "./ModeToggle";
+import { Button } from "./ui/button";
 
 const Navbar = () => {
   return (
     <div className=" sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur  ">
       <div className=" container flex h-14 items-center  ">
-        <h1>Github Jobs</h1>
+        <h1 className="text-xl">
+          <span className=" font-bold text-2xl ">Github </span>Jobs
+        </h1>
         <div className="flex-1"></div>
-        <ModeToggle />
+        <div className="flex gap-2">
+          <ModeToggle />
+          <Button variant="default">Sign in</Button>
+        </div>
       </div>
     </div>
   );
