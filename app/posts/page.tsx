bbca9bb622deea6adc68a5a9f1ed161a9a86b0d5
prasannakarki77 <prasanna.karki77@gmail.com>
2023-08-18 12:06:29 +0545

    added loading page and component

--- app/posts/page.tsx
+++ app/posts/page.tsx
@@ -1,3 +1,4 @@
+import Loader from "@/components/Loader";
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
 import React from "react";
@@ -6,6 +7,7 @@ const Posts = () => {
   return (
     <div className="container my-5">
       <Link href="/posts/create">Create Job posting</Link>
+      <Loader />
     </div>
   );
 };
