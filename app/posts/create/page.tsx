d3e163c9ba335e29ebd3c149da541937c0ef0de6
prasannakarki77 <prasanna.karki77@gmail.com>
2023-08-18 11:47:15 +0545

    added form heading

--- app/posts/create/page.tsx
+++ app/posts/create/page.tsx
@@ -1,8 +1,11 @@
 import JobPostingForm from "@/components/forms/JobPostingForm";
+import { Separator } from "@/components/ui/separator";
 
 const CreateJobPosting = () => {
   return (
-    <div className="container my-5">
+    <div className="container my-5  max-w-5xl">
+      <h1 className="text-2xl font-bold my-3">Add Job Posting</h1>
+      <Separator className="my-4" />
       <JobPostingForm />
     </div>
   );
