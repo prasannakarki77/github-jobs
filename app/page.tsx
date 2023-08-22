44e2a0364ebcd98ecfd0392b5ab5297421fef569
prasannakarki77 <prasanna.karki77@gmail.com>
2023-08-22 20:26:43 +0545

    added search bar to home page

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import getPosts from "./actions/getPosts";
 import ClientOnly from "@/components/ClientOnly";
 import EmptyState from "@/components/EmptyState";
 import JobPostCard from "@/components/JobPostCard";
+import SearchBar from "@/components/SearchBar";
 
 export default async function Home() {
   const posts = await getPosts();
@@ -14,10 +15,16 @@ export default async function Home() {
   }
   return (
     <ClientOnly>
-      <div className="flex flex-col gap-4">
-        {posts.map((post) => {
-          return <JobPostCard key={post.id} post={post} />;
-        })}
+      <div className=" ">
+        <div className=" mb-4 bg-search-bg h-40 flex justify-center items-center px-10 ">
+          <SearchBar />
+        </div>
+
+        <div className="flex flex-col gap-10">
+          {posts.map((post) => {
+            return <JobPostCard key={post.id} post={post} />;
+          })}
+        </div>
       </div>
     </ClientOnly>
   );
--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -5,9 +5,13 @@ import { Input } from "./ui/input";
 
 const SearchBar = () => {
   return (
-    <div className=" flex gap-2 max-w-md   ">
-      <Input type="text" placeholder="Email" />
-      <Button size={"default"}>Search</Button>
+    <div className="flex gap-2 max-w-xl flex-1">
+      <Input
+        type="text"
+        placeholder="Job title"
+        className=" dark:bg-secondary shadow-md"
+      />
+      <Button>Search</Button>
     </div>
   );
 };
