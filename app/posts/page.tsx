2bd67ae491fc39400d3e0411c402f6a07a0b6cb4
prasannakarki77 <prasanna.karki77@gmail.com>
2023-08-22 21:40:28 +0545

    added create new job post button

--- app/posts/page.tsx
+++ app/posts/page.tsx
@@ -2,6 +2,10 @@ import React from "react";
 import { Metadata } from "next";
 import getPosts from "../actions/getPosts";
 import { DataTable } from "@/components/posts/PostsTable";
+import { Button, buttonVariants } from "@/components/ui/button";
+import Link from "next/link";
+import { cn } from "@/lib/utils";
+import { PlusIcon } from "lucide-react";
 
 export const metadata: Metadata = {
   title: "Job Posts | GitHub Jobs",
@@ -12,7 +16,15 @@ const Posts = async () => {
 
   return (
     <div className=" ">
-      <h1 className=" font-bold">Job Posts</h1>
+      <div className=" flex justify-between items-center mb-5">
+        <h1 className=" font-bold text-xl">Job Posts</h1>
+        <Link
+          href={"/posts/create"}
+          className={cn(buttonVariants({ variant: "default" }))}
+        >
+          <PlusIcon size={18} className="mr-2" /> New Job Post
+        </Link>
+      </div>
       <DataTable data={posts} />
     </div>
   );
