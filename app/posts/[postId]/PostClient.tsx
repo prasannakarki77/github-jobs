083d902bebd1793692cc13491f0e1c5e37e2167f
prasannakarki77 <prasanna.karki77@gmail.com>
2023-08-25 20:49:43 +0545

    displayed employer info in job post

--- app/posts/[postId]/PostClient.tsx
+++ app/posts/[postId]/PostClient.tsx
@@ -1,7 +1,9 @@
 import { Badge } from "@/components/ui/badge";
-import { fDate } from "@/lib/utils";
+import { fDate, getInitials } from "@/lib/utils";
 import { SafePost } from "@/types/prisma";
-import { TimerIcon } from "lucide-react";
+import { GlobeIcon, TimerIcon } from "lucide-react";
+import Image from "next/image";
+
 interface PostClientProps {
   post: SafePost;
 }
@@ -19,7 +21,7 @@ const PostClient: React.FC<PostClientProps> = ({ post }) => {
   } = post;
 
   return (
-    <div className="flex  flex-col gap-3">
+    <div className="flex  flex-col gap-6">
       <div>
         <div className="flex items-center gap-2">
           <h1 className="text-xl font-bold mb-1">{title}</h1>
@@ -31,6 +33,30 @@ const PostClient: React.FC<PostClientProps> = ({ post }) => {
           <TimerIcon size={15} />
           {fDate(createdAt)}
         </p>
+      </div>
+      <div className=" flex gap-2">
+        <div className=" bg-gray-300 dark:bg-slate-800 w-14  rounded-sm ">
+          {user.image ? (
+            <Image
+              src={user.image}
+              height={60}
+              width={80}
+              alt={user.id}
+              className="w-full h-full object-cover"
+            />
+          ) : (
+            <p className=" w-full h-full flex justify-center items-center font-bold text-xl">
+              {user.name && getInitials(user?.name)}
+            </p>
+          )}
+        </div>
+        <div className="flex flex-col gap-2">
+          <span className=" font-bold">{user.name}</span>
+          <span className="text-xs text-slate-400 font-semibold flex gap-1 items-center">
+            <GlobeIcon size={15} />
+            {location}
+          </span>
+        </div>
       </div>
       <div
         dangerouslySetInnerHTML={{ __html: description }}
