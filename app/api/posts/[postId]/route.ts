f6b776cacdce0230276cb975138f0332605057c0
prasannakarki77 <prasanna.karki77@gmail.com>
2023-08-28 20:53:15 +0545

    created update post api route

--- app/api/posts/[postId]/route.ts
+++ app/api/posts/[postId]/route.ts
@@ -32,3 +32,30 @@ export async function DELETE(
 
   return NextResponse.json(post);
 }
+
+export async function PUT(request: Request, { params }: { params: IParams }) {
+  const currentUser = await getCurrentUser();
+
+  if (!currentUser || currentUser.role !== UserRole.EMPLOYER) {
+    return NextResponse.error();
+  }
+
+  const { postId } = params;
+  if (!postId || !Number(postId)) {
+    throw new Error("Invalid Id");
+  }
+  const body = await request.json();
+  const { title, description, location, duration, expiresAt } = body;
+
+  const post = await prisma.posting.update({
+    where: { id: Number(postId) },
+    data: {
+      title,
+      description,
+      location,
+      duration,
+      expiresAt,
+    },
+  });
+  return NextResponse.json(post);
+}
