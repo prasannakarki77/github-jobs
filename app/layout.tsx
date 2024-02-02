8fa911ce91c4c853b228e4bacabc70bc66951c91
prasannakarki77 <prasanna.karki77@gmail.com>
2024-02-02 22:36:13 +0545

    removed unwanted code

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,7 +14,38 @@ export const metadata: Metadata = {
   title: "GitHub Jobs: Find your future jobs",
   description: "Search various tech jobs on listed on GitHub Jobs",
 };
-
+// openGraph: {
+//   title: "InternSathi 1.0 Coming Soon - Join Waitlist Now",
+//   description:
+//     "Become a part of the InternSathi community by signing up for our Private Beta. Gain access to our groundbreaking platform and start exploring internships like never before.",
+//   url: "https://internsathi.com",
+//   type: "website",
+//   siteName: "InternSathi 1.0 Coming Soon - Join Waitlist Now",
+//   locale: "en_US",
+//   images: [
+//     {
+//       url: "https://internsathi.com/internsathi-og.png",
+//       width: 1200,
+//       height: 630,
+//       alt: "InternSathi 1.0 Logo",
+//     },
+//   ],
+// },
+// twitter: {
+//   card: "summary",
+//   site: "@InternSathi",
+//   images: [
+//     {
+//       url: "https://internsathi.com/internsathi-og.png",
+//       width: 1200,
+//       height: 630,
+//       alt: "InternSathi 1.0 Logo",
+//     },
+//   ],
+//   title: "InternSathi 1.0 Coming Soon - Join Waitlist Now",
+//   description:
+//     "Become a part of the InternSathi community by signing up for our Private Beta. Gain access to our groundbreaking platform and start exploring internships like never before.",
+// },
 export default async function RootLayout({
   children,
 }: {
