7c6d2d4281cc98546912481c9c5bdaf8a085943e
prasannakarki77 <prasanna.karki77@gmail.com>
2024-02-16 17:39:44 +0545

    top loader color black

--- app/layout.tsx
+++ app/layout.tsx
@@ -26,7 +26,7 @@ export default async function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className} min-h-screen bg-background font-sans>
-        <NextTopLoader />
+        <NextTopLoader color="#000" />
         <Provider>
           <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
             <ClientOnly>
